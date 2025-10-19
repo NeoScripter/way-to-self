a652e04961516f506d90d07bc44a9c423ce60b13
Neoscripter <sange0337@gmail.com>
2025-10-19 19:23:00 +0800

    add quote cron job

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+})->purpose('Display an inspiring quote')->everyMinute();
 
 Schedule::command('model:prune')->weekly();
 
