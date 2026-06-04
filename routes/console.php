814dba5520fcbc401f8b3782d5b23aaa2dbd0ecb
Neoscripter <sange0337@gmail.com>
2026-06-04 12:38:22 +0800

    Debug:add no tablespaces to the db dump command

--- routes/console.php
+++ routes/console.php
@@ -20,7 +20,7 @@ Artisan::command('backup_database', function (): void {
     $password = config('database.connections.mysql.password');
     $path = storage_path('app/private/backup.sql');
 
-    exec("mysqldump -u {$username} --password={$password} {$database} > {$path}");
+    exec("mysqldump -u {$username} --password={$password} {$database} > {$path} --no-tablespaces");
 
     $gzdata = gzencode(file_get_contents($path));
     file_put_contents("{$path}.gz", $gzdata);
